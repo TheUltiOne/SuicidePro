7de5f2c77003a4869b550b84ee17949ecde4018d
TheUltiOne <ultioneofficial@gmail.com>
2022-04-04 21:37:25 -0400

    StringBuilder

--- Handlers/KillCommand.cs
+++ Handlers/KillCommand.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using CommandSystem;
 using Exiled.API.Features;
 using Exiled.Permissions.Extensions;
@@ -32,19 +33,20 @@ namespace SuicidePro.Handlers
 
 			if (Plugin.Instance.Config.HelpCommandAliases.Contains(arg))
 			{
-				response = "Here are all the kill commands you can use:\n\n";
+				var build = new StringBuilder("Here are all the kill commands you can use:\n\n");
 				foreach (var commandConfig in Plugin.Instance.Config.KillConfigs)
 				{
 					if (commandConfig.Permission == "none" || player.CheckPermission(FormatPermission(commandConfig)))
-						response += $"<b><color=white>.{Plugin.Instance.Config.CommandPrefix}</color> <color=yellow>{commandConfig.Name}</color></b> {(commandConfig.Aliases.Any() ? $"<color=#3C3C3C>({String.Join(", ", commandConfig.Aliases)})</color>" : String.Empty)}\n<color=white>{commandConfig.Description}</color>\n\n";
+						build.Append($"<b><color=white>.{Plugin.Instance.Config.CommandPrefix}</color> <color=yellow>{commandConfig.Name}</color></b> {(commandConfig.Aliases.Any() ? $"<color=#3C3C3C>({String.Join(", ", commandConfig.Aliases)})</color>" : String.Empty)}\n<color=white>{commandConfig.Description}</color>\n\n");
 				}
 
 				foreach (var effect in CustomEffect.CustomEffect.Effects)
 				{
 					if (effect.Config.Enabled && (effect.Config.Permission == "none" || player.CheckPermission(FormatPermission(effect.Config))))
-						response += $"<b><color=white>.{Plugin.Instance.Config.CommandPrefix}</color> <color=yellow>{effect.Config.Name}</color></b> {(effect.Config.Aliases.Any() ? $"<color=#3C3C3C>({String.Join(", ", effect.Config.Aliases)})</color>" : String.Empty)}\n<color=white>{effect.Config.Description}</color>\n\n";
+						build.Append($"<b><color=white>.{Plugin.Instance.Config.CommandPrefix}</color> <color=yellow>{effect.Config.Name}</color></b> {(effect.Config.Aliases.Any() ? $"<color=#3C3C3C>({String.Join(", ", effect.Config.Aliases)})</color>" : String.Empty)}\n<color=white>{effect.Config.Description}</color>\n\n");
 				}
 
+				response = build.ToString();
 				return true;
 			}
 
